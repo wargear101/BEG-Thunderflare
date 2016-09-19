264ed84a62687999bcfae26feb62e152b9b07f93
LPorsemo <Linus.porsemo@gmail.com>
2016-09-19 09:25:30 +0200

    Update BEG.js

--- BEG.js
+++ BEG.js
@@ -1,11 +1,14 @@
-$(document).ready(function(){
-	$.get("http://ofcourse.oru.se/~IK2009/json/get_jobs.php", function(data){
- if(data.status == 1)
- {		
- 	$.each(data.result, function() {
- 		$('#jobPosition').append('<li class="job">' + this.city + ', ' + this.position + '</li>');
- 	});
- }
- 				
-	}
-	)})
+
+$(document).ready(function () {
+    $.get("http://ofcourse.oru.se/~IK2009/json/get_jobs.php", function (data) {
+        if (data.status == 1) {
+            $.each(data.result, function () {
+                $('#jobPosition').append('<li class="job">' + this.city + ', <button class="showDesc"> ' + this.position + ' </button>, <p class="testa" style="display: none;">' + this.description + '</p>', '</li>');
+            });
+        }
+    });
+
+    $(document).on('click', '.showDesc', function () {
+        $(this).next().toggle();
+    });
+});
